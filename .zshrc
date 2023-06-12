79dba279fa40f7f790c975a2195eeac867415145
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-06-12 03:35:35 -0300

    updated links and other files

--- .zshrc
+++ .zshrc
@@ -1,4 +1,4 @@
-source ~/antigen.zsh
+source ~/.antigen.zsh
 # Load the oh-my-zsh's library.
 antigen use oh-my-zsh
 
@@ -34,7 +34,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="agnoster"
+ZSH_THEME="agnoster" # set by `omz`
 
 
 # Set list of themes to pick from when loading at random
--- bash/scripts/setup.sh
+++ bash/scripts/setup.sh
@@ -8,6 +8,24 @@ then
     sudo dnf install git -y
 fi
 
+# Check if zsh is installed
+if ! command -v zsh &> /dev/null
+then
+    echo "Zsh is not installed. Installing..."
+    sudo dnf install zsh -y
+fi
+
+# Check if chsh is available
+if ! command -v chsh &> /dev/null
+then
+    echo "'chsh' is not found. Installing 'util-linux-user'..."
+    sudo dnf install util-linux-user -y
+fi
+
+# Change default shell to zsh
+echo "Changing default shell to zsh..."
+chsh -s $(which zsh)
+
 # Clone dotfiles repository
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Cloning dotfiles repository..."
@@ -18,7 +36,8 @@ fi
 echo "Creating symlinks..."
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/bash/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.antigen.zsh ~/.antigen.zsh
 
 # Install Zsh and oh-my-zsh
 $HOME/.dotfiles/bash/scripts/install_zsh_and_oh_my_zsh.sh
